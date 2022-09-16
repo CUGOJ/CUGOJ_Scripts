ac84cf6618b320d8789db0cf2638175218bfa081
zhaoxiao <504315513@qq.com>
2022-09-17 01:01:48 +0800

    Core发布脚本

--- deploy-core.sh
+++ deploy-core.sh
@@ -6,6 +6,7 @@
 echo '正在构建项目'
 mkdir -p ~/cugoj/src
 mkdir -p ~/cugoj/output/CUGOJ_Core
+rm -rf ~/cugoj/output/CUGOJ_Core
 cd ~/cugoj/src
 git clone -b $3 https://ghproxy.com/https://github.com/CUGOJ/CUGOJ_Core.git
 cd CUGOJ_Core
@@ -14,12 +15,13 @@ rm -rf output
 sh ./build.sh
 mv output/* ~/cugoj/output/CUGOJ_Core/
 
-echo `构建成功`
-echo `正在部署项目`
+echo '构建成功'
+echo '正在部署项目'
 
-mkdir -p ~/cugoj/deploy/$1/CUGOJ_Core
-cp -r ~/cugoj/output/CUGOJ_Core/* ~/cugoj/deploy/$1/CUGOJ_Core/
-cd ~/cugoj/deploy/$1/CUGOJ_Core/
+mkdir -p ~/cugoj/deploy/$1/CUGOJ_Core/output
+rm -rf ~/cugoj/deploy/$1/CUGOJ_Core/output/*
+cp -r ~/cugoj/output/CUGOJ_Core/* ~/cugoj/deploy/$1/CUGOJ_Core/output/
+cd ~/cugoj/deploy/$1/CUGOJ_Core/output
 
 sh run.sh $1 $2 &
 
